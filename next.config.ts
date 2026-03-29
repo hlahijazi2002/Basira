0dab1ff6477aa73d46fe267d5ef5ae7cc3a694f7
hlahijazi2002 <hlahijazi22@gmail.com>
2026-03-29 20:30:02 +0300

    Performance Fix: Optimized LCP, fixed scroll links, and removed heavy dynamic imports

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   compress: true,
-  images: {
-    formats: ["image/avif", "image/webp"],
-  },
   // هذا السطر يساعد في تقليل الـ JavaScript المرسل للمتصفح
   experimental: {
     optimizePackageImports: ["lucide-react", "framer-motion"],
