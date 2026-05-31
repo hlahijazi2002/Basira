f669cd10dd2f7fb9cc152100fb4488bb670c587a
hlahijazi2002 <hlahijazi22@gmail.com>
2026-05-31 21:15:03 +0300

    merge conflict resolved

--- app/page.tsx
+++ app/page.tsx
@@ -8,14 +8,14 @@ import FloatingChat from "@/components/FloatingChat";
 
 export default function Home() {
   return (
-    <main className="relative bg-slate-950">
-      <Hero />
-      <About />
-      <Team />
-      <Services />
-      <Contact />
-      <Footer />
-      <FloatingChat />
-    </main>
+    <h1>test</h1>
+    // <main className="relative bg-slate-950">
+    //   <Hero />
+    //   <About />
+    //   <Team />
+    //   <Services />
+    //   <Contact />
+    //   <Footer />
+    // </main>
   );
 }
